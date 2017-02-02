72b299fc212daeced7965b11a2390430383db7ad
Brayden <btraas@gmail.com>
2017-02-02 12:41:49 -0800

    Initial commit

---
+++ app/src/main/java/a00968178/comp3717/bcit/ca/opendata/CategoriesActivity.java
@@ -0,0 +1,16 @@
+package a00968178.comp3717.bcit.ca.opendata;
+
+import android.app.ListActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CategoriesActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categories);
+    }
+
+
+}
