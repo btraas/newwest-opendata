392df1a06313dec2fe8f8863c584c7e5b0a1727b
Brayden <btraas@gmail.com>
2017-02-02 14:43:01 -0800

    fixed navigation directions

--- app/src/main/java/a00968178/comp3717/bcit/ca/opendata/DatasetsActivity.java
+++ app/src/main/java/a00968178/comp3717/bcit/ca/opendata/DatasetsActivity.java
@@ -2,29 +2,30 @@ package a00968178.comp3717.bcit.ca.opendata;
 
 import android.app.ListActivity;
 import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class CategoriesActivity extends ListActivity {
+public class DatasetsActivity extends ListActivity {
 
-    String[] list = Categories.getNames();
-    private static final String TAG = CategoriesActivity.class.getName();
+
+    String[] list = {"test1", "test2", "test3"};
+
+    private static final String TAG = DatasetsActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_categories);
-
-
+        setContentView(R.layout.activity_datasets);
 
         setListAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, list));
-
-        Log.d(TAG, "onCreate end");
     }
+
 
     @Override
     public void onListItemClick(ListView list, final View view, int position, long id)
@@ -34,14 +35,16 @@ public class CategoriesActivity extends ListActivity {
         String text = ((TextView)view).getText().toString();
 
 
-        final Intent intent;
-        intent = new Intent(this, DatasetsActivity.class);
+        //final Intent intent;
+        //intent = new Intent(this, DatasetsActivity.class);
 
         //Bundle b = new Bundle();
-        intent.putExtra("name", text);
+        //intent.putExtra("name", text);
         //intent.putExtras(b);
 
-        startActivity(intent);
+        //startActivity(intent);
+
+        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
 
         Log.d(TAG, "onItemClick end");
     }
